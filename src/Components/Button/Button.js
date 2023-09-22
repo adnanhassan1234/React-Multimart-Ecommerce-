de3f0b60b69fa28a03626af6002f9073dd3c2571
adnanhassan1234 <ah5404219@gmail.com>
2023-09-22 23:39:51 +0500

    checkout

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './button.scss';
 import { motion } from 'framer-motion';
 
-const Button = ({title ,submit}) => {
+const Button = ({title ,submit, width}) => {
     return (
         <>
-            <motion.button whileTap={{scale: 1.3}} type={submit} className='btn btn-primary'>{title}</motion.button>
+            <motion.button whileTap={{scale: 1.3}} type={submit} className='btn btn-primary' style={{width:width}}>{title}</motion.button>
         </>
     );
 };
--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Helmet from "../../Components/Helmet/Helmet";
 import CommonSection from "../../Components/UI/CommonSection";
 import "./cart.scss";
@@ -9,19 +9,19 @@ import Button from "../../Components/Button/Button";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const dispatch =  useDispatch();
+  const dispatch = useDispatch();
   const cartItem = useSelector((state) => state.carts.cartItem);
   const totalAmount = useSelector((state) => state.carts.totalAmount);
 
   const deleteItem = (id) => {
-   dispatch(removeItem(id))
-   toast.success('Product deleted successfully!');
-  }
+    dispatch(removeItem(id));
+    toast.success('Product deleted successfully!');
+  };
 
   const removeAllItem = () => {
     dispatch(clearCart());
-    toast.success('All Product deleted successfully!');
-  }
+    toast.success('All Products deleted successfully!');
+  };
 
   return (
     <Helmet title="Cart">
@@ -29,18 +29,13 @@ const Cart = () => {
       <section className="cart my-5">
         <div className="container">
           {cartItem.length === 0 ? (
-            <div> 
-              <h2 className="text-center mx-5 fs-3">No Item in Cart!</h2>
-            </div> 
-          ) : null}
-          <div className="row">
-            <div className="col-lg-9">
-              <div className="cart_details">
-                {cartItem.length === 0 ? (
-                  <div>
-                    <h2 className="d-none "></h2>
-                  </div>
-                ) : (
+            <div className="text-center mx-5 fs-3">
+              <h4>No Items in Cart!</h4>
+            </div>
+          ) : (
+            <div className="row">
+              <div className="col-lg-9">
+                <div className="cart_details">
                   <table className="table bordered">
                     <thead>
                       <tr>
@@ -52,49 +47,51 @@ const Cart = () => {
                       </tr>
                     </thead>
                     <tbody>
-                      {cartItem?.map((item, index) => {
-                        return (
-                          <tr key={index}>
-                            <td>
-                              <img src={item.imgUrl} alt="" />
-                            </td>
-                            <td>{item.productName}</td>
-                            <td>{item.price}</td>
-                            <td>{item.quantity}</td>
-                            <td onClick={() => deleteItem(item.id)}>
-                              <i className="ri-delete-bin-6-line"></i>
-                            </td>
-                          </tr>
-                          
-                        );
-                      })}
-                     <div className="clear_all_item my-3">
-                     <button type="button" className="btn btn-danger" onClick={removeAllItem}>Remove all Item</button>
-                     </div>
+                      {cartItem.map((item) => (
+                        <tr key={item.id}>
+                          <td>
+                            <img src={item.imgUrl} alt="" />
+                          </td>
+                          <td>{item.productName}</td>
+                          <td>{item.price}</td>
+                          <td>{item.quantity}</td>
+                          <td onClick={() => deleteItem(item.id)}>
+                            <i className="ri-delete-bin-6-line"></i>
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
-                )}
+                  <div className="clear_all_item my-3">
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={removeAllItem}
+                    >
+                      Remove all Items
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3">
+                <div className="sub_total">
+                  <div className="mt-3 d-flex justify-content-between">
+                    <h5>Subtotal</h5>
+                    <span><b>${totalAmount}</b></span>
+                  </div>
+                  <div className="taxes my-3">
+                    <p>Taxes and shipping will be calculated at checkout</p>
+                  </div>
+                  <div className="shopping_btn mt-4">
+                    <Link to="/shop"><Button title="Continue Shopping" width="100%" /></Link>
+                  </div>
+                  <div className="shopping_btn my-3">
+                    <Link to="/checkout"><Button title="Checkout" width="100%" /></Link>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-lg-3">
-              <div className="sub_total">
-               <div className="mt-3 d-flex justify-content-between">
-               <h5>Subtotal</h5>
-                <span><b>${totalAmount}</b></span>
-               </div>
-             
-              <div className="taxes my-3">
-                <p>Taxes and shipping will calculate in  checkout</p>
-              </div>
-              <div className="shopping_btn my-3">
-                <Link to="/shop"><Button title="Continue Shopping" /></Link>
-              </div>
-              <div className="shopping_btn my-3">
-              <Link to="/checkout"> <Button title="Checkout" /></Link>
-              </div>
-            </div>
-          </div>
-          </div>
+          )}
         </div>
       </section>
     </Helmet>
